60f73805e4eab84ad562d41237537100f6b56906
Beco <ghewqa1234@gmail.com>
2024-10-17 03:50:14 +0900

    Fix hp/sp job factor bug
    - JOB_REFERENCE.SOX -> if main job is advanced then treats novice job level as 19 (nCommandJobMod)

--- sl/emulator/game/system/player_stat_system.cpp
+++ sl/emulator/game/system/player_stat_system.cpp
@@ -154,7 +154,7 @@ namespace sunlight
         const Job* novice = jobComponent.Find(JobType::Novice);
         const Job* advanced = jobComponent.Find(JobType::Advanced);
 
-        const int32_t levelSum = (novice ? novice->GetLevel() : 0) + (advanced ? advanced->GetLevel() : 0);
+        const int32_t levelSum = (novice ? novice->GetLevel() : 0) + (advanced ? advanced->GetLevel() - 1 : 0);
         const sox::JobReference* data = advanced ? &advanced->GetData() : (novice ? &novice->GetData() : nullptr);
 
         if (!data)
