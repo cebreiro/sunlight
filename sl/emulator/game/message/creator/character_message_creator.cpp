9b47007d5f47f024ed39ef56b10df7f6f63d830a
Beco <ghewqa1234@gmail.com>
2024-10-31 08:53:48 +0900

    synchronize party name in profile window

--- sl/emulator/game/message/creator/character_message_creator.cpp
+++ sl/emulator/game/message/creator/character_message_creator.cpp
@@ -2,6 +2,7 @@
 
 #include "sl/emulator/game/component/player_appearance_component.h"
 #include "sl/emulator/game/component/player_job_component.h"
+#include "sl/emulator/game/component/player_party_component.h"
 #include "sl/emulator/game/component/player_profile_component.h"
 #include "sl/emulator/game/component/player_stat_component.h"
 #include "sl/emulator/game/component/scene_object_component.h"
@@ -87,10 +88,20 @@ namespace sunlight
             objectWriter.Write<int32_t>(static_cast<int32_t>(position.x()));
             objectWriter.Write<int32_t>(static_cast<int32_t>(position.y()));
 
-            objectWriter.WriteFixeSizeString("", 128); // no reference
+            objectWriter.WriteFixeSizeString("", 128); // no usage
             objectWriter.WriteFixeSizeString(zone.zoneDesc, 256); // position
-            objectWriter.WriteFixeSizeString("TODO: party", 32); // TODO: party
-            objectWriter.WriteFixeSizeString("", 32); // no reference 
+
+            if (const auto& partyComponent = targetPlayer.GetPartyComponent();
+                partyComponent.HasParty())
+            {
+                objectWriter.WriteFixeSizeString(partyComponent.GetPartyName(), 32);
+            }
+            else
+            {
+                objectWriter.WriteFixeSizeString("", 32);
+            }
+
+            objectWriter.WriteFixeSizeString("", 32); // no usage
 
             writer.WriteObject(objectWriter);
         }
