486824e68b84f368eefc5752578a61e19f7ec243
Beco <ghewqa1234@gmail.com>
2024-11-13 14:41:59 +0900

    Center tile position

--- sl/emulator/game/contents/path_finding/tile.cpp
+++ sl/emulator/game/contents/path_finding/tile.cpp
@@ -6,6 +6,12 @@ namespace sunlight
 {
     auto Tile::GetPosition() const -> Eigen::Vector2f
     {
-        return { static_cast<float>(index.x) * GameConstant::TILE_SIZE, static_cast<float>(index.y) * GameConstant::TILE_SIZE };
+        // client 0x490AC5
+        constexpr float median = GameConstant::TILE_SIZE / 2.f;
+
+        const float x = static_cast<float>(index.x) * GameConstant::TILE_SIZE + median;
+        const float y = static_cast<float>(index.y) * GameConstant::TILE_SIZE + median;
+
+        return { x, y };
     }
 }
